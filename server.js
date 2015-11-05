977553a1351774cf53e4670d41828a5d1ab3913e
TGAStudent <tga.student@telegraphacademy.com>
2015-11-04 16:02:20 -0800

    Changed package to rid sqlite err mess

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ var port = 4568;
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
+
+// Team NotAsleep: "no changes seem needed on process.env file (inspected)." //console.log('Process.env:', process.env);
