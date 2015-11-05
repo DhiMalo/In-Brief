dc354ba9efe908c85442de46a0bf12e98f26311f
TGAStudent <tga.student@telegraphacademy.com>
2015-11-04 16:45:30 -0800

    modify app.config

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@ var path = require('path');
 var db = Bookshelf.initialize({
   client: 'sqlite3',
   connection: {
-    host: 'http://shortylink.azurewebsites.net/',
+    host: '127.0.0.1',
     user: 'your_database_user',
     password: 'password',
     database: 'shortlydb',
